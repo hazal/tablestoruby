b1742a1df717a792d58eb2778f99019d49e61a24
niyazi ates <niyaziates77@gmail.com>
2012-10-02 23:51:37 +0300

    comments for code finished and some words to readme for explanation

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 tablestoruby
 ============
 
-make a new table with using two text file in table form
+make a new table with using two text file in table form.
+
+here we have table1 for midterm results . we firstly calculate average of this midterm results.
+Then we have table2 has final results.
+Using these tables we calculate GRADES , LETTER GRADES and other results of Courses.
+
+pls. do not hesitate to give your comments. thanks.	
--- courses.rb
+++ courses.rb
@@ -1,108 +1,81 @@
 # Read the midterm1 and midterm2
 #grades = Array.new(10){ Array.new(10,0) }
+
 midterm = Array.new(4){ Array.new(4,0) } # 4 rows for courses , 4 columns for coursename , mt1 , mt2 and mt_ave
 File.readlines('midterms.txt').each_with_index do|line,idx|
   mt_list = line.chomp.split(' ')
-#  puts eldenlist [3] # 4. sütunu yazdýrýr <<
-#  puts student[2] >> her satırın 3. öğesini yazdırır <<
   # Merge into existing array
-  puts midterm.length # >> her satýrýn eleman sayýsýný verir <<
-  midterm[idx][0..mt_list.length - 1] = mt_list # kdcc
+  midterm[idx][0..mt_list.length - 1] = mt_list 
   end
-# puts eldenlist >> hata verir "undefined local variable" ama grades dizi olduðu için mi çýkýyor ? <<
-# puts elden[9][11]  #>> 10. satýrýn 12. sütununu verir <<
 
-# Convert from strings to integers. Sayý görünen stringler tamsayý yapýlacak
+# Convert from strings to integers.
 
 midterm.each do|g|
-  (1..2).each do|i| # 0. eleman isim olduðu için almaık
+  (1..2).each do|i| #  g[0] course name 
     g[i] = g[i].to_i
 	end
 end
 
-#puts elden.length
-# toplamý hesapla
+# put and calculate last column as midtherm average
 midterm.each do|g|
-g[g.length-1] = g[1..g.length-2].reduce(:+) / 2 # satýr sonuna bir kolon ekliyor
-# puts g  >> yeni oluþturulmuþ son kolon da dahil her satýrý yazdýrýr <<
-# puts g.last
+g[g.length-1] = g[1..g.length-2].reduce(:+) / 2 
 end
-# Output the final grades
-puts "%-14s %4s" % [ "COURSES", "MT_AVE"]
+
+# Output the mid_term averages
+puts "%-14s %4s" % [ "COURSES", "MT_Ave"] # formatted HEADER
 midterm.each do|g|
-  puts "%-14s %4d" % [ g.first, g.last ] # adý ve soyadýný verecek þekilde deðiþtir
+  puts "%-14s %4d" % [ g.first, g.last ] 
 end
+
 # **********************   second part *********************
+
 results = Array.new(4){ Array.new(6,0) } # 4 rows , 6 columns
 File.readlines('result.txt').each_with_index do|line,idx|
   finallist = line.chomp.split(' ')
-#  puts eldenlist [3] # 4. sütunu yazdýrýr <<
-#  puts student[2] >> her satýrýn 3. öðesini yazdýrýr <<
   # Merge into existing array
-#  puts student.length >> her satýrýn eleman sayýsýný verir <<
   results[idx][0..finallist.length - 1] = finallist
- # puts maas
   end
-# puts eldenlist >> hata verir "undefined local variable" ama grades dizi olduðu için mi çýkýyor ? <<
-# puts maas[9][3]  #>> 10. satýrýn 4. sütununu verir <<
-#puts maas
+# Convert from strings to integers.
 results.each do|m|
-(1..3).each do|i| # 0. eleman ÝSÝM olduðu için almadýk
+(1..3).each do|i| # m[0] course name
     m[i] = m[i].to_i
 	end
 end
-# puts maas.length
-#elden.each do |g|
 
-#elden.each do|g|
-  puts "%-8s %8s %4s %4s %4s %4s" % [ "COURSES" , "FINAL" , "MT_Ave" , "GRADE" , "LETTER" , "PASS/NOT"] 
-#end
+# formatted HEADER
+puts "%-8s %8s %4s %4s %4s %4s" % [ "COURSES" , "FINAL" , "MT_Ave" , "GRADE" , "LETTER" , "PASS/NOT"] 
 
-
- 
+# importand we use nested .each method to use column of table1 into table2
 results.each do |m| 
-#puts m[0]
-midterm.each do |g|
-#puts g.last
-#(0..9).each do|i|
-#puts g.last
-if m[0]== g[0] # sadece isimler aynı olması durumunda elden alınan tablosunun son kolonunu maaş tablosunda 5. kolona yerleştiriyoruz
-m[2]= g.last  # m[2] : maas, m[3]: resmisi , m[4] : elden aldığı
-#end
-#m[m.length-5] = m[2]-m[4] 
-end
-end
-#puts m[4]
-m[m.length-3] = (m[2]*40/100)+(m[1]*60/100) # m[5] : kalan alacağı (maaş - elden aldığı)
-m[m.length-2] = case m[m.length-3]
-when 0..59
-  "FF"
-when 60..69
-  "DD"
-when 70..79
-  "CC"
-when 80..89
-  "BB"
-else
-  "AA"
-end
-=begin
-if m[m.length-6] <= m[3] # kalanı resmiden az veya eşit ise
-	m[m.length-5] = m[m.length-6] # bankaya kalanın tamamı ödenir
-	else
-	m[m.length-5] = m[3] # yada bankaya resmisi ödenir
-	end
-	m[m.length-4] = m[m.length-6] - m[m.length-5] # elden alacağı maaş
-m[m.length-3] = m[4]+m[6]+m[7]
-=end
-if m[m.length-3] >= 70
-m[m.length-1] = "ok"
-end
-#if m[m.length-6] < m[m.length-8]
-#m[m.length-1] = m[m.length-8] - m[m.length-5]
-#end
-#puts maas
-end
+  midterm.each do |g|
+    if m[0]== g[0] # we want to take mt_ave of courses which have SAME NAME
+      m[2]= g.last  # last clmn of table1 inserted into 3rd clmn of table2
+    end
+  end
+
+m[m.length-3] = (m[2]*40/100)+(m[1]*60/100) # grade points : 40% of mt_ave and 60% of final
+
+# use of CASE below to convert grade point to LETTER GRADE
+    m[m.length-2] = case m[m.length-3]
+    when 0..59
+      "FF"
+    when 60..69
+      "DD"
+    when 70..79
+      "CC"
+    when 80..89
+      "BB"
+    else
+      "AA"
+    end
+
+# lastly it calculate to say you PASS the Courses :) or not :(
+  if m[m.length-3] >= 70
+    m[m.length-1] = "ok"
+  end
+end # this is "end" of line 49 : results.each do |m|
+
+# and the table :
 results.each do|m|
  puts "%-10s %4d %4d %6d %6s %6s " % [ m[0],m[1],m[2],m[3],m[4],m[5]] 
 end#puts maas
